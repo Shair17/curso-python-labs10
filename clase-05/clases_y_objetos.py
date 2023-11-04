654a26ceea893d951f93f475ee437ea351c50245
Shair17 <jimmy_jhair17@hotmail.com>
2023-11-03 20:51:04 -0500

    clase 05 - encapsulamiento

--- clase-05/clases_y_objetos.py
+++ clase-05/clases_y_objetos.py
@@ -1,16 +1,52 @@
 # Definición de una clase
-class Coche:
-    def __init__(self, marca, modelo):
-        self.marca = marca
-        self.modelo = modelo
-    
-    def describir_coche(self):
-        return f"Este coche es un {self.marca} {self.modelo}"
+class Carro:
+  def __init__(self, marca, modelo, cantidad_llantas, maxima_velocidad):
+    self.marca = marca
+    self.modelo = modelo
+    self.cantidad_llantas = cantidad_llantas
+    self.maxima_velocidad = maxima_velocidad
+
+  def describir_coche(self) -> str:
+    return f"Este carro es un {self.marca} {self.modelo}"
+
+  def obtener_maxima_velocidad(self) -> str:
+    return f"Este carro tiene una máxima velocidad de {self.maxima_velocidad}km/h"
 
 # Creación de objetos a partir de la clase Coche
-coche1 = Coche("Toyota", "Corolla")
-coche2 = Coche("Ford", "Mustang")
+carro1 = Carro("Toyota", "Corolla", 4, 300)
+carro2 = Carro("Ford", "Mustang", 4, 400)
 
 # Uso de métodos de los objetos
-print(coche1.describir_coche())  # Output: Este coche es un Toyota Corolla
-print(coche2.describir_coche())  # Output: Este coche es un Ford Mustang
+print(carro1.describir_coche())  # Output: Este coche es un Toyota Corolla
+print(carro2.describir_coche())  # Output: Este coche es un Ford Mustang
+
+print(carro1.obtener_maxima_velocidad())
+print(carro2.obtener_maxima_velocidad())
+
+# Crear un cajero automático con POO, en el cajero se puede establecer la cantidad de dinero que hay disponible, se puede retirar y depositar dinero. Además debería de poder mostrar la cantidad de dinero disponible.
+
+class Cajero:
+  def __init__(self, saldo):
+    self.saldo = saldo
+
+  def depositar(self, cantidad_deposito):
+    self.saldo += cantidad_deposito
+    print(f"Se ha depositado {cantidad_deposito}")
+
+  def retirar(self, cantidad_retiro):
+    if self.saldo >= cantidad_retiro:
+      self.saldo -= cantidad_retiro
+      print(f"Se ha retirado {cantidad_retiro}")
+    else:
+      print(f"Dinero insuficiente")
+
+  def mostrar_saldo(self):
+    print(f"Saldo actual: {self.saldo}")
+
+cajero_1 = Cajero(1000)
+cajero_1.mostrar_saldo()
+cajero_1.depositar(500)
+cajero_1.retirar(1500)
+cajero_1.mostrar_saldo()
+
+# Crear un objeto de una persona y en base a eso crear distintas instancias con datos o acciones según tu criterio
--- clase-05/encapsulamiento.py
+++ clase-05/encapsulamiento.py
@@ -1,13 +1,17 @@
 class Coche:
-    def __init__(self, marca, modelo):
-        self.__marca = marca  # __ indica que es privado
-        self.__modelo = modelo
+  def __init__(self, marca, modelo):
+    self.__secreto = "secreto"
+    self.__marca = marca  # __ indica que es privado
+    self.__modelo = modelo
 
-    def get_marca(self):
-        return self.__marca
+  def obtener_secreto(self):
+    return self.__secreto[::-1]
 
-    def set_marca(self, nueva_marca):
-        self.__marca = nueva_marca
+  def get_marca(self):
+    return self.__marca
+
+  def set_marca(self, nueva_marca):
+    self.__marca = nueva_marca
 
 # Uso de encapsulamiento
 coche = Coche("Toyota", "Corolla")
@@ -19,3 +23,7 @@ print(coche.get_marca())  # Output: Toyota
 # Modificación a través de un método
 coche.set_marca("Ford")
 print(coche.get_marca())  # Output: Ford
+
+print(coche.obtener_secreto())
+
+# Crear una instancia de un producto el cual tiene ingredientes publicos e ingredientes secretos, luego mostrar esos ingredientes secretos pero de manera encriptada
