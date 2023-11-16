d9aca9834e45c964c486a658674d07daddcc719f
Shair17 <jimmy_jhair17@hotmail.com>
2023-11-15 20:36:50 -0500

    clase 7 - estructura de nuestro blog

---
+++ clase-07/mi-web/app.js
@@ -0,0 +1,10 @@
+const app = document.getElementById("app");
+
+const headings = ["h1", "h2", "h3", "h4", "h5", "h6"];
+
+for (const headingElement of headings) {
+  const el = document.createElement(headingElement);
+  el.innerText = `Esto es una etiqueta de html ${headingElement}`;
+
+  app.appendChild(el);
+}
